113395742e834603d60b8591d7085df6bffb4d7f
DiegoGCV <73501708+DiegoGCV@users.noreply.github.com>
2020-11-03 00:13:50 -0600

    Arreglo de Regla AV 42 y 37

--- Usuarios.h
+++ Usuarios.h
@@ -7,7 +7,10 @@ using namespace std;
 
 class Usuario{
 public:
-    string id, nombre, apellido, ruta;
+    string id;
+    string nombre; 
+    string apellido
+    string ruta;
     string arrayNombres[20]= {"Ana", "Roberto", "Juan", "Rodrigo", "Angel",
                             "Agustin", "Cristian", "Carlos", "Julian", "Belen",
                             "Jonathan", "Israel", "Jose", "Luis", "Miguel",
@@ -26,46 +29,5 @@ public:
     void UsuarioArray(int jala);
     Usuario *siguiente, *anterior;
 };
-
-void firstUpper ( string& str ) {
-  str[0] = ::toupper( str[0] );
-  for ( string::iterator it = str.begin(); it != str.end(); it++ )
-    if ( *it == ' ' )
-      *( it+1 ) = ::toupper( *( it+1 ) );
-}
-void Usuario::Ingresar()
-{
-    cout<<"Ingrese el id: "<<endl;
-    fflush(stdin);
-    getline(cin, id);
-    cout<<"Ingrese el nombre: "<<endl;
-    fflush(stdin);
-    getline(cin,nombre);
-    firstUpper(nombre);
-    cout<<"Ingrese el apellido: "<<endl;
-    fflush(stdin);
-    getline(cin, apellido);
-    firstUpper(apellido);
-    cout<<"Ingrese la ruta: "<<endl;
-    fflush(stdin);
-    getline(cin,ruta);
-}
-
-void Usuario::Mostrar()
-{
-    cout<<"El id es: "<<id<<endl;
-    cout<<"El nombre es: "<<nombre<<endl;
-    cout<<"El apellido es: "<<apellido<<endl;
-    cout<<"La ruta es: "<<ruta<<endl;
-    cout<<"|||||||||||||||||||||||||"<<endl;
-}
-
-void Usuario::UsuarioArray(int jala)
-{
-id=arrayId[jala];
-nombre=arrayNombres[jala];
-apellido=arrayApellido[jala];
-ruta=arrayRuta[jala];
-}
 
 #endif // USUARIOS_H_INCLUDED
